fffb2b1ea13a1a4218037183cb2dc3d1867e9e43
timdh94 <timothyh94@gmail.com>
2021-10-08 18:46:04 -0400

    Added new models to db init function.

--- server/src/models/init.ts
+++ server/src/models/init.ts
@@ -1,12 +1,15 @@
 import User from './user';
 import Achievement from './achievement';
 import AchievementTemplate from './achievementTemplate';
+import ActiveQuest from './activeQuest';
+import Quest from './quest';
 
 const dbInit = async () => {
   await User.sync();
   await AchievementTemplate.sync();
   await Achievement.sync();
+  await ActiveQuest.sync();
+  await Quest.sync();
 };
-
 
 export default dbInit;
