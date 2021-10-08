f965091018296c1e5ff389c2c98b3e75c9f7698b
Sean Araujo <seanaraujo93@gmail.com>
2021-10-08 16:03:29 -0400

    Front end component navigation

---
+++ Client/Screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View, Button, StyleSheet} from 'react-native';
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>Home Page!</Text>
+      <Button title="Go to Profile" />
+    </View>
+  );
+};
+
+export default HomeScreen;
