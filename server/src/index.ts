2b87c98bfe61d57e274643d52469ea1590dc4c76
timdh94 <timothyh94@gmail.com>
2021-10-08 18:42:40 -0400

    Updated imports and initialized db on server start

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
 import express from 'express';
 // import routers
-import cfg from './config';
+import cfg from '../config';
 import dbInit from './models/init';
 
 const app = express();
 
 app.use(express.json());
-app.listen(cfg.PORT, () => {
-  dbInit();
+app.listen(cfg.PORT, async () => {
+  await dbInit();
   console.log(`Server listening on port ${cfg.PORT}`);
 });
