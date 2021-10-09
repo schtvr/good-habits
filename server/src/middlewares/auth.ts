aa8a5d1dde17e5f34be2b68cc57e7188f975d9fc
timdh94 <timothyh94@gmail.com>
2021-10-09 16:54:42 -0400

    Improved auth

--- server/src/middlewares/auth.ts
+++ server/src/middlewares/auth.ts
@@ -1,29 +1,29 @@
-import jwt, { Jwt, JwtPayload } from 'jsonwebtoken';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 import config from '../../config';
 import { Request, Response, NextFunction } from 'express';
-import User, { IUser } from '../models/user';
+import User from '../models/user';
 
-interface IVerifiedRequest extends Request{
-  user: IUser | undefined
+export interface IVerifiedRequest extends Request{
+  user: User | undefined
 }
 
 const verify = (req: IVerifiedRequest, res: Response, next: NextFunction) => {
   const authHeader = req.headers['authorization'];
-  const token = authHeader && authHeader.split(' ')[1]
-  if(!token){
+  const token = authHeader && authHeader.split(' ')[1];
+  if (!token) {
     return res.status(403).send('Unauthorized');
   }
-  jwt.verify(token, config.SECRET as string, async (err: any, payload: JwtPayload | undefined) => {
-    if (err) return res.sendStatus(403)
+  jwt.verify(token, config.SECRET, async (err: any, payload: JwtPayload | undefined) => {
+    if (err) return res.sendStatus(403);
     if (!payload || !payload.userId) return res.status(403).send('Was not passed userId');
     const { userId } = payload;
     const user: User | null = await User.findOne({
       where: { id: userId }
     });
-    if(!user) return res.sendStatus(404);
+    if (!user) return res.sendStatus(404);
     req.user = user;
 
-    next()
+    return next();
   });
 };
 
