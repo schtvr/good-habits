3514573a6f78ee583ce3a83542a19a766c2e4275
timdh94 <timothyh94@gmail.com>
2021-10-10 10:22:53 -0400

    Created basic controllers for achievements, quests, tasks, and users.

---
+++ server/src/controllers/task.ts
@@ -0,0 +1,25 @@
+import Task from '../models/task';
+import { Request, Response } from 'express';
+
+const getTaskById = async (req: Request, res:Response) => {
+  if (!req.params.id) return res.status(422).send('Missing taskId');
+  const foundTask = await Task.findOne({
+    where: {
+      id: req.params.id
+    }
+  });
+  res.json(foundTask);
+}
+
+const completeTaskById = async (req: Request, res:Response) => {
+  if (!req.user) return res.status(400).send('Not authenticated');
+  if (!req.body.taskId) return res.status(422).send('Missing taskId');
+  const foundTask = await req.user.getTaskHistory({ where: { id: req.body.taskId }});
+  const result = foundTask.complete();
+  if (result) console.log('poo poo');
+};
+
+export default {
+  getTaskById,
+  completeTaskById
+};
