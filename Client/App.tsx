42b79c874b156fe2bcbcfcfc4b94342b65cd808e
Juan T. Palacios <juante92@gmail.com>
2021-10-11 13:28:22 -0300

    Added header buttons

--- Client/App.tsx
+++ Client/App.tsx
@@ -4,10 +4,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import type {Node} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-import {StyleSheet} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
-
-
 
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
@@ -19,13 +17,24 @@ import AchievementsPage from './screens/AchievementsPage';
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
+const headerRight = () => (
+  <View style={styles.header}>
+    <TouchableOpacity onPress={() => alert('Notifications!')}>
+      <Text style={styles.icons}>🔔</Text>
+    </TouchableOpacity>
+    <TouchableOpacity onPress={() => alert('Search!')}>
+      <Text style={styles.icons}>🔍</Text>
+    </TouchableOpacity>
+  </View>
+);
+
 const TabStack = () => (
   <Tab.Navigator initialRouteName="Home">
-    <Tab.Screen name="Home" component={HomeScreen} />
-    <Tab.Screen name="Profile" component={ProfileScreen} />
-    <Tab.Screen name="Social" component={SocialScreen} />
-    <Tab.Screen name="Leaderboards" component={LeaderBoardScreen} />
-    {/* <Tab.Screen name="Settings" component={SettingsScreen} /> */}
+    <Tab.Screen name="Home" component={HomeScreen} options={{headerRight}}/>
+    <Tab.Screen name="Profile" component={ProfileScreen} options={{headerRight}}/>
+    <Tab.Screen name="Social" component={SocialScreen} options={{headerRight}}/>
+    <Tab.Screen name="Leaderboards" component={LeaderBoardScreen} options={{headerRight}}/>
+    {/* <Tab.Screen name="Settings" component={SettingsScreen} /> options={{headerRight}} */}
   </Tab.Navigator>
 );
 
@@ -34,18 +43,27 @@ const App: () => Node = () => {
     <SafeAreaProvider>
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen
-            options={{headerShown: false}}
-            name="Main"
-            component={TabStack}
-          />
-          <Stack.Screen name="Achievements" component={AchievementsPage} />
+        <Stack.Screen
+        options={{headerShown: false}}
+        name="Main"
+        component={TabStack}
+        />
+        <Stack.Screen name="Achievements" component={AchievementsPage} options={{headerRight}}/>
         </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+        </NavigationContainer>
+      </SafeAreaProvider>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    paddingRight: 30,
+  },
+  icons: {
+    margin: 10,
+    fontSize: 20
+  }
+});
 
 export default App;
