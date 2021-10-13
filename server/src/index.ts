376b45c1375f5dba4b8df904dfca019153bb3deb
Victor Klawitter <victor.klawitter@gmail.com>
2021-10-13 10:49:30 -0400

    Added cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,18 @@ import express from 'express';
 import cfg from '../config';
 import dbInit from './models/init';
 import router from './router';
+import cors from 'cors';
 import populateDb from './funcs/populatedb';
 const app = express();
 
+const corsConfig = {
+  origin: 'http://localhost:3000/',
+};
+
+app.use(cors());
 app.use(express.json());
 app.use(router);
+
 app.listen(cfg.PORT, async () => {
   await dbInit();
   await populateDb();
