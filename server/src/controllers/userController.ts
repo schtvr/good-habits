9d35223350a8a78ab90cfc98241a860eb915861d
Victor Klawitter <victor.klawitter@gmail.com>
2021-10-13 12:55:25 -0400

    Added accept friend request

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -74,7 +74,7 @@ const findUserById = async (req: Request, res: Response) => {
 
 // router.put('/user/:id/friendRequest',verify ,user.putFriendRequest);
 // router.get('/user/friendRequestReceived', verify ,user.getFriendRequestReceived);
-// router.get('/user/:id/friendRequestSent', verify ,user.getFriendRequestSent);
+// router.get('/user/friendRequestSent', verify ,user.getFriendRequestSent);
 // router.put('/user/:id/acceptFriendRequest', verify ,user.acceptFriendRequest);
 // router.get('/user/:id/friends', verify ,user.getFriends);
 // router.put('/user/:id/cancelFriendRequest', verify ,user.cancelFriendRequest);
@@ -158,6 +158,41 @@ const getFriendRequestSent = async (req: Request, res: Response) => {
   }
 };
 const acceptFriendRequest = async (req: Request, res: Response) => {
+  const userName = req.params.id;
+  const user = req.user;
+  if (!userName || !user) return res.status(403).send({
+    status: 'Bad',
+    message: 'Missing userId or not authenticated',
+  });
+  try { 
+    const userToFriend = await User.findOne({
+      where: {
+        userName
+      }
+    });
+    if (!userToFriend) return res.status(404).send({
+      status: 'Bad',
+      message: 'You sent me a user that doesn\'t exist dumdum',
+    });
+
+    const checkFriendRequests = await user.hasRequestee(userToFriend.id);
+    if (!checkFriendRequests) return res.status(404).send({
+      status: 'Bad',
+      message: 'BaBaBOi they didn\'t even send a friend request',
+    });
+    await user.removeRequestee([userToFriend.id]);
+    await user.addFriends(userToFriend.id);
+    return res.status(200).send({
+      status: 'Okay',
+      message: 'Friend request accepted'
+    });
+  } catch (err) {
+    res.status(500).send({
+      status: 'Bad',
+      message: 'Internal Server Error',
+      data: err,
+    }); 
+  }
 };
 const getFriends = async (req: Request, res: Response) => {
 };
