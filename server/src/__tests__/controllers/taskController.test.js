469edf1e7d43bba4e14d61c48c3456ebd4984d9e
timdh94 <timothyh94@gmail.com>
2021-10-13 13:07:49 -0400

    Started working on task controller tests

--- server/src/__tests__/controllers/taskController.test.js
+++ server/src/__tests__/controllers/taskController.test.js
@@ -9,7 +9,7 @@ import Quest from '../../models/quest';
 import Task from '../../models/task';
 import TaskHistory from '../../models/taskHistory';
 import populateDb from '../../funcs/populatedb';
-import AchievementTemplate from '../../models/achievementTemplate';
+import Achievement from '../../models/achievement';
 
 describe ('Task Controller', () => {
   const app = express();
@@ -17,8 +17,48 @@ describe ('Task Controller', () => {
   app.use(router);
   
   const request = supertest(app);
+
+  let user;
+  let quest;
+  let tasks;
+  let loginRes;
+
+  beforeAll(async () => {
+    await dbInit();
+    await populateDb();
+    
+    user = await User.findOne({
+      where: {
+        userName: 'timbo'
+      }
+    });
+    quest = await Quest.findOne({
+      where: {
+        name: 'Mediation crash course'
+      }
+    }); 
+    loginRes = await request.post('/login').send({
+      emailOrUserName: 'bigBoy@gmail.com',
+      password: 'password123'
+    });
+    tasks = await quest.getTasks();
+  });
+  
+  afterAll(async () => {
+    await sequelize.drop(); 
+  });
   
   test('retrieve a quest\'s tasks', async () => {
-
+    const res = await request.get(`/tasks/quest/${quest.id}`);
+    expect(res.body.data).toHaveLength(quest.taskCount);
+  });
+  
+  test('complete a task', async () => {
+    const res = await request.post(`/tasks/${tasks[0].id}`).set(
+      'Authorization',
+      `Bearer ${loginRes.body.data}`
+    );
+    const chieves = await user.getAchievements();
+    expect(chieves).toHaveLength(1);
   });
 });
