e9ef87d77ad4f39f1d3e72b440054463faba7cc6
Juan T. Palacios <juante92@gmail.com>
2021-10-13 15:50:46 -0300

    login working and api service middleware is error handling

--- Client/redux/apiService.ts
+++ Client/redux/apiService.ts
@@ -1,10 +1,10 @@
 import {LOCALURL} from 'react-native-dotenv';
 
 const apiService = store => next => action => {
-  console.log('inside apiService', action);
+  console.log('inside apiService', action.body);
   if (!action?.payload?.api) return next(action);
   const api = action.payload.api;
-
+  let type = action.type;
   const method = api.method || 'GET';
   const body = api.body ? JSON.stringify(api.body) : undefined;
   const defaultHeaders = {'Content-Type': 'application/json'};
@@ -12,18 +12,22 @@ const apiService = store => next => action => {
     ...defaultHeaders,
     ...api.headers,
   };
-  console.log('headers', headers);
-  next({type: `${action.type}_REQUEST`});
-
+  console.log(method, body, headers);
   fetch(`${LOCALURL}/${api.url}`, {method, body, headers})
-    .then(res => res.json())
-    .then(data => {
-      console.log('data', data);
-      store.dispatch({type: action.type, data});
-    })
-    .catch(error => {
-      store.dispatch({type: action.type, error});
-    });
+  .then(res => res.json())
+  .then(data => {
+    if (data.status === 'Bad') {
+      type = 'user/error'
+      store.dispatch({type: type, data});
+      return;
+    }
+    store.dispatch({type: type, data});
+  })
+  .catch(error => {
+    type = 'user/error';
+    store.dispatch({type: type, error});
+  });
+  // return next({type: type});
 };
 
 export default apiService;
