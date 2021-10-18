de521bc129084c464aab1f49f4baf9623258a82b
timdh94 <timothyh94@gmail.com>
2021-10-18 11:03:51 -0400

    Started working on markign old tasks incomplete.

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -7,6 +7,9 @@ import TaskHistory from '../models/taskHistory';
 import getDaysApart from '../funcs/getDailyTasks/getDaysApart';
 import addTasks from '../funcs/getDailyTasks/addTasks';
 import checkQuestCompleted from '../funcs/checkQuestCompleted';
+import ActiveQuest from '../models/activeQuest';
+import Quest from '../models/quest';
+import User from '../models/user';
 
 const getTaskById = async (req: Request, res:Response) => {
   try {
@@ -56,6 +59,7 @@ const completeTaskById = async (req: Request, res:Response) => {
       exp: user.exp += update.gainedExp
     });
     
+    await fixOldTasks(user);
     return sendRes(res, true, 200, 'Task completed', update);
   } catch (err) {
     return sendRes(res, false, 500, 'Server error completing task', err);
@@ -82,6 +86,7 @@ const getTaskHistory = async (req: Request, res: Response) => {
   try {
     const user = req.user;
     if (!user) return sendRes(res, false, 403, 'Not a valid user');
+
     const taskHistory = await user.getTaskHistory();
     return sendRes(res, true, 200, 'Task history retrieved', taskHistory);
   } catch (err) {
@@ -90,6 +95,7 @@ const getTaskHistory = async (req: Request, res: Response) => {
 };
 
 const getDailyTasks = async (req: Request, res: Response) => {
+  console.log('DAILY CALL');
   const user = req.user;
   if (!user) return sendRes(res, false, 403, 'Not a valid user');
 
@@ -110,6 +116,113 @@ const getDailyTasks = async (req: Request, res: Response) => {
     sendRes(res, false, 500, 'Server error getting daily tasks');
   }
 };
+
+
+const fixOldTasks = async (user: User) => {
+  
+  
+  // get task history
+  // get completed & active quests
+  
+  
+  // active quest ---> get tasks
+  // get user completed tasks for that quest ---->
+  // find difference between start of quest and now - 1
+  // for each task of quest from start to index
+  // if task not in completed task --->
+  // create task history with completed = false
+  
+  const activeQuests = await getUserActiveQuests(user);
+  const currDate = Date.now();
+  for await (const quest of activeQuests) {
+    const currDay = getDaysApart(currDate, quest.startDate.getTime()) - 1;
+
+    const tasks = await getActiveQuestTasks(quest);
+
+    const completedTasks = await getCompletedTasksForQuest(user, quest);
+    const completed = getCompletedIds(completedTasks); 
+    
+    
+    for (const task of tasks) {
+      if ((task.day < currDay) && !completed[task.id]) {
+        console.log('MARKIGN TASK INCOMPLETE');
+        await user.createTaskHistory({
+          taskId: task.id,
+          questId: task.questId,
+          completed: false
+        });
+      }
+    }
+  }
+
+  // need to know active quests && their tasks
+  // need to know what tasks the user has completed for each quest
+  // need to know what day of the quest they are on
+  // check for any missing tasks in between current day and task history
+};
+
+const getTaskDays = (tasks: Task[]) => {
+  const taskDays = [];
+  for (const task of tasks) {
+    taskDays.push(task.day);
+  }
+  return taskDays;
+};
+
+const getCompletedIds = (history: TaskHistory[]) => {
+  const scuffedObj: any = {};
+  for (const task of history) {
+    scuffedObj[task.taskId] = true;
+  }
+  return scuffedObj;
+};
+
+const getUserActiveQuests = async (user: User) => {
+  try {
+    const activeQuests = await user.getActiveQuests();
+    return activeQuests;
+  } catch (err) {
+    return [];
+  }
+};
+
+const getActiveQuestTasks = async (active: ActiveQuest) => {
+  try {
+    const tasks = await Task.findAll({
+      where: {
+        questId: active.questId
+      }
+    });
+    return tasks;
+  } catch (err) {
+    return [];
+  }
+};
+
+const getCompletedTasksForQuest = async (user: User, active: ActiveQuest) => {
+  try {
+    const userHistory = await user.getTaskHistory({
+      where: {
+        questId: active.questId
+      }
+    });
+    return userHistory;
+  } catch (err) {
+    return [];
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default {
   getTaskById,
