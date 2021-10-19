ac676488cc49ac4432ad73b3aae6a2907c54771a
steve rigby <stevenrigby.me@gmail.com>
2021-10-19 15:11:57 -0400

    changed styling on quests screen

--- client/screens/QuestsScreen.tsx
+++ client/screens/QuestsScreen.tsx
@@ -6,13 +6,15 @@ import {
   FlatList,
   ImageSourcePropType,
 } from 'react-native';
-import {View, Text, StyleSheet, SafeAreaView} from 'react-native';
-import {Card, FAB, Icon} from 'react-native-elements';
+import {View, Text, StyleSheet, SafeAreaView, ImageBackground} from 'react-native';
+import {Card, FAB, Icon, ThemeProvider} from 'react-native-elements';
 import {useDispatch, useSelector} from 'react-redux';
 import {friendSelector} from '../redux/friendSlice';
 import { getQuests } from '../funcs/dispatch/dispatchFuncs'
 import { questSelector } from '../redux/questSlice';
 import { useIsFocused } from '@react-navigation/core';
+import { elementsTheme } from '../styles/react-native-elements-theme-provider'
+import QuestDetailCard from '../components/QuestDetailCard';
 
 interface IQuest {
   category: string;
@@ -35,59 +37,45 @@ const QuestsScreen = ({navigation}) => {
   const {quests} = useSelector(questSelector);
   const dispatch = useDispatch();
 
-  
-  
+
+
   const renderItems: ListRenderItem<IQuest> = ({item, index}) => {
+    console.log('ITEM', item)
     return (
-      <>
-        <TouchableOpacity onPress={() => navigation.navigate('QuestDetailsScreen', {id: item.id})}>
-          <Card containerStyle={{flexDirection: 'row'}}>
-            <View style={styles.image}>
-              <Image
-                resizeMode="cover"
-                source={require('../assets/friend1.png')}
-              />
-              <View style={styles.userInfo}>
-                <Text style={styles.title}>{item.name}</Text>
-                <Text style={styles.level}>Duration {item.duration} days</Text>
-                <Text style={styles.level}>Number of Tasks: {item.taskCount}</Text>
-              </View>
-            </View>
-            <Card.Divider
-              color={'black'}
-              inset={true}
-              insetType={'middle'}
-              subHeader={item.description}
-              subHeaderStyle={styles.text}
-            />
-          </Card>
-        </TouchableOpacity>
-      </>
-    );
+    <>
+      <TouchableOpacity onPress={() => navigation.navigate('QuestDetailsScreen', {id: item.id})}>
+        <QuestDetailCard id={item.id} />
+      </TouchableOpacity>
+     </>
+    )
   };
-  
+
   return (
-    <SafeAreaView style={{flex: 1, paddingBottom: 45}}>
-       <FAB style={{zIndex: 5}} placement="right" color='#6e85ec'  icon={<Icon size={25} name={"add"} color="white"/>}
-        onPress={() => navigation.navigate('Create Quest')}
-       />
-      <View>
-        <Text style={styles.title}>Find new Quests!</Text>
-       
-        
-        <FlatList
-          data={quests}
-          keyExtractor={item => item.id.toString()}
-          renderItem={renderItems}
-        />
-      </View>
-    </SafeAreaView>
+    <ThemeProvider theme={elementsTheme}>
+       <ImageBackground
+        style={{flex: 1}}
+        source={require('../assets/mauve-stacked-waves-haikei.png')}>
+      <SafeAreaView style={{flex: 1}}>
+         <FAB style={{zIndex: 5}} placement="right" color={'peru'}  icon={<Icon size={25} name={"add"} color="white"/>}
+          onPress={() => navigation.navigate('Create Quest')}
+         />
+        <View>
+
+
+          <FlatList
+            data={quests}
+            keyExtractor={item => item.id.toString()}
+            renderItem={renderItems}
+          />
+        </View>
+      </SafeAreaView>
+      </ImageBackground>
+    </ThemeProvider>
   );
 };
 
 const styles = StyleSheet.create({
   title: {
-    alignSelf: 'center',
     fontWeight: 'bold',
     fontSize: 22,
   },
@@ -98,7 +86,7 @@ const styles = StyleSheet.create({
     paddingTop: 20,
   },
   text: {
-    fontSize: 20,
+    fontSize: 14,
     fontWeight: 'bold',
   },
   questName: {
@@ -106,12 +94,9 @@ const styles = StyleSheet.create({
   },
   level: {
     fontSize: 16,
-    fontWeight: 'bold',
     paddingLeft: 10,
-    paddingTop: 10,
   },
   image: {
-    justifyContent: 'center',
     flexDirection: 'row',
     alignItems: 'center',
   },
